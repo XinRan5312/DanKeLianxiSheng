1a02b288bdffa78271e9c8f20f90a719218bb9bb
qixinh <qixinh@jumei.com>
2018-02-03 14:11:10 +0800

    友盟分享https://github.com/wildma/UMengThirdPartyShareLogin

---
+++ app/src/main/java/com/xinran/studyindanke/lifecyle_mvp/LoginVew.java
@@ -0,0 +1,25 @@
+package com.xinran.studyindanke.lifecyle_mvp;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+
+/**
+ * Created by houqixin on 2018/2/1.
+ */
+
+public class LoginVew extends LinearLayout {
+    public LoginVew(Context context) {
+        super(context);
+    }
+
+    public LoginVew(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LoginVew(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
